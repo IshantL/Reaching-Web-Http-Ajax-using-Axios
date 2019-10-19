aa0ba84b40723a045462ae734e33e8a88cfa3216
IshantL <ishantlambhate@gmail.com>
2019-10-19 19:16:54 +0530

    adding interceptor , global configration of axios

--- src/index.js
+++ src/index.js
@@ -3,6 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+axios.interceptors.request.use(request => {
+    debugger;
+    request.data ={
+        ...request.data,
+        author: "ishant"
+    }
+    console.log(request);
+    // Edit request config
+    return request;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+    debugger;
+    console.log(response);
+    // Edit request config
+    return response;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
